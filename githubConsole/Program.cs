4ad46ee54dd75b673e1b94de33468efc90410de1
ProfBlomme <kianlhaynes@gmail.com>
2024-03-18 08:42:34 +0100

    This should give an error

--- githubConsole/Program.cs
+++ githubConsole/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Lets test merging with coworkers!");
+            Console.WriteLine("This should give us an error");
+
         }
     }
 }
