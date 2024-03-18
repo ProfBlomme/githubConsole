990eae72e071b650c6d278737858c3b71cea1e9b
jannicksostack <142694256+jannicksostack@users.noreply.github.com>
2024-03-18 10:07:35 +0100

    Update Program.cs

--- githubConsole/Program.cs
+++ githubConsole/Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("test");
             Console.WriteLine("test");
             Console.WriteLine("qwe");
-            Console.WriteLine("test");
+            Console.WriteLine("asdasd");
         }
     }
 }
